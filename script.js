66a9b483d51bdd86e1919a4c43141846f700105b
Utkir <utkirkurbanov2700@gmail.com>
2022-09-29 15:54:29 +0500

    JavaScript Data Types

---
+++ script.js
@@ -0,0 +1,19 @@
+/*
+ Number
+ String
+ Boolean
+ Object
+ Null
+ Undefined
+*/
+
+var num = 42;
+var string = "message";
+var isTrue = true; // false
+var obj = {
+  a: 1,
+};
+var nothing = null;
+var undef = undefined;
+
+console.log(typeof undef);
